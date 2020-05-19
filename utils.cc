785d209ecea4100ddc739d1847221cbf1ad877bb
Ian Hinder <ian.hinder@manchester.ac.uk>
2020-05-19 20:19:32 +0100

    utils.cc: Improve code formatting

--- utils.cc
+++ utils.cc
@@ -11,13 +11,25 @@
 using namespace std;
 
 /// Draws a random number between 0 and 1
-double ran(){
-	if(RAND_MAX == 32767){
-		double v = (double(rand())*32766.0+rand())/(32767.0*RAND_MAX); if(v == 0 || v == 1) return 0.1;
-		else return v;
+double ran()
+{
+	if(RAND_MAX == 32767) {
+		double v = (double(rand())*32766.0+rand())/(32767.0*RAND_MAX);
+		if(v == 0 || v == 1) {
+			return 0.1;
+		}
+		else {
+			return v;
+		}
 	}
-	else return double(0.999999999*rand())/RAND_MAX;
+	else {
+		return double(0.999999999*rand())/RAND_MAX;
+	}
 }
 
 /// Displays any error messages
-void emsg(string msg){ cout << msg << endl; exit (EXIT_FAILURE);}
+void emsg(string msg)
+{
+	cout << msg << endl;
+	exit (EXIT_FAILURE);
+}
