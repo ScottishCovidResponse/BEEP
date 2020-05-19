7891f1373d949d2ade7adc08952b37320a60b325
Ian Hinder <ian.hinder@manchester.ac.uk>
2020-05-19 19:01:00 +0100

    Move poptree types into poptree.hh

--- poptree.hh
+++ poptree.hh
@@ -1,6 +1,34 @@
 
 #pragma once
 
+struct HOUSE {                             // Defines a house
+ 	double x, y;                             // Position
+	vector <long> ind;                       // Individuals which belong to the house
+};
+
+struct NODE {                              // Provides information about a node
+	vector <long> houseref;                  // References the list of houses within the node
+	long parent;                             // The parent node
+	vector <long> child;                     // The child nodes
+	vector <long> fine;                      // The child nodes on the fine scale
+	long popu;                               // The total population in the node
+	double x, y;                             // The position of the node (given by the average of all the houses)
+	short done;                              // A flag used to determine if this node has been analysed or not
+};
+
+struct LEVEL {                             // Stores information about different levels 
+	vector <NODE> node;                      // The nodes at a given level
+ 	vector <long> donelist;                  // List of nodes which have been processed
+	vector <double> add;                     // Used when adding up the tree
+};
+
+struct IND {                               // Provides information about an individual
+	long noderef;                            // The node on the finescale to which the individual belongs
+	long houseref;                           // The house to which the individual belongs
+	short region;                            // The region to which the individual belongs
+};
+
+
 struct HouseRefComparatorX
 {
 	public:
