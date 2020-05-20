adceb39f74f1c16f37df333a4a0ac86baaf79350
Chris Pooley <chris.pooley@roslin.ed.ac.uk>
2020-05-20 17:45:44 +0100

    Parameters typical of the UK

--- consts.hh
+++ consts.hh
@@ -5,15 +5,23 @@ const short EXT_EV=3;
 const double tiny = 0.00000001;            // Used to represent a tiny number
 const double large = 10000000;             // Used to represent a big number
 const double invT = 1;                     // The inverse temperature (used to relax the observation model)
-const long popsize = 5500000;              // Number of individuals in Scotland (approximately)
-const long nhouse = 1500000;               // Number of houses
+
+//const long popsize = 68000000;              // Numbers Typical of UK
+//const long nhouse = 22000000;  
+//const short RX = 13, RY = 13;            
+    
+const long popsize = 5500000;              // Numbers Typical of Scotland
+const long nhouse = 1500000;  
+const short RX = 4, RY = 4;            
+    
 //const long popsize = 10000;                 // A smaller test case
-//const long nhouse = 3000;               
+//const long nhouse = 3000;
+//const short RX = 4, RY = 4;   
+		 
 const short checkon = 0;                   // Set to one to check algorithm is performing correctly
 const double finegridsize = 0.02;          // The range in distance over which the fine grid is used 
 const double d0 = 0.05;                    // The minumum distance cut-off for the matrix M
 const short tmax = 105;                    // The time over which simulation / inference is performed
-const short RX = 4, RY = 4;                // When siumlating this gives a hypothetical grid of regions
 const short nregion = RX*RY;               // When the real data is analysed these will be Healthboard level regions
 const long fediv = 1000;                   // The number of divisions into which the global timeline is divided
 const long partmax = 10000;                // The maximum number of particles (arbitrarily set)
