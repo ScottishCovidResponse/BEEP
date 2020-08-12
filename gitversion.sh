4b70581f4d6805c37637af8ca0e588002363419c
Robin Williams <67799396+rwj11@users.noreply.github.com>
2020-08-12 09:10:19 +0100

    Deal with issues reported in gitversion.sh

--- gitversion.sh
+++ gitversion.sh
@@ -7,17 +7,17 @@ outfile=$1
 tmpfile=${outfile}.tmp
 
 version=$(git describe --dirty --always)
-cat >$tmpfile <<EOF
+cat >"$tmpfile" <<EOF
 #pragma once
 #define GIT_VERSION "$version"
 EOF
 
 if [ -r $outfile ]; then
-    if diff -q $outfile $tmpfile >/dev/null; then
-        rm -f $tmpfile
+    if diff -q "$outfile" "$tmpfile" >/dev/null; then
+        rm -f "$tmpfile"
         # No change to version
         exit 0
     fi
 fi
 
-mv $tmpfile $outfile
+mv "$tmpfile" "$outfile"
