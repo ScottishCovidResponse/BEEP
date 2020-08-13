6f214fa48901d461664e5b7ae866aba72df5ee2c
Robin Williams <67799396+rwj11@users.noreply.github.com>
2020-08-13 10:24:47 +0100

    Re-enable remaining unit tests

--- codetests/test_utils.cc
+++ codetests/test_utils.cc
@@ -9,64 +9,66 @@
 
 using namespace std;
 
-/*
-TEST_CASE("test_ran") {
-
+const char* suite_random = "Random numbers";
+TEST_CASE("ran after seed set to 0 returns result expected for std::mt19937",
+					suite_random) {
 	sran(0);
 	double r = ran();
-
+	
 //	cout << "r == " << std::setprecision(19) << r << endl;
-
-  CHECK(r == 0.5928446159645536229);
+	
+	REQUIRE(r == 0.5928446159645536229);
 }
 
-TEST_CASE("split") {
-	vector<string> parts = split("ab*defgh**p*", '*');
 
+const char* suite_string = "String utilities";
+
+TEST_CASE("split breaks up const char*",suite_string) {
+	vector<string> parts = split("ab*defgh**p*", '*');
+	
 	CHECK(parts.at(0) == "ab");
 	CHECK(parts.at(1) == "defgh");
 	CHECK(parts.at(2) == "");
 	CHECK(parts.at(3) == "p");
-	CHECK(parts.size() == 4);
-
+	REQUIRE(parts.size() == 4);
+	
 	// ianhinder: I would have expected the following to work, but the last part
 	// is not returned
 	// CHECK(parts.at(4) == "");
 	// CHECK(parts.size() == 5);
 }
-*/
 
 ///////////////////////////////////////////////////////////////////////////
 // getint
 ///////////////////////////////////////////////////////////////////////////
 
-const char* suite = "[data.cc:getint()]";
-TEST_CASE("getint reads an integer",suite) {
+
+TEST_CASE("getint reads an integer",suite_string) {
 	unsigned int i = getint("42", UNSET);
 	REQUIRE(i == 42);
 }
-
-TEST_CASE("getint reads 'NA' as UNKNOWN",suite) {
+	
+TEST_CASE("getint reads 'NA' as UNKNOWN",suite_string) {
 	unsigned int i = getint("NA", UNSET);
 	REQUIRE(i == UNKNOWN);
 }
 
-TEST_CASE("getint reads '*' as THRESH if threshold is given",suite) {
+TEST_CASE("getint reads '*' as THRESH if threshold is given",suite_string) {
 	unsigned int i = getint("*", 5);
 	REQUIRE(i == THRESH);
 }
 
-TEST_CASE("getint fails on '*' if no threshold is given",suite) {
+TEST_CASE("getint fails on '*' if no threshold is given",suite_string) {
 	unsigned int i;
 	REQUIRE_THROWS_AS(i = getint("*", UNSET),std::runtime_error);
 }
 
-TEST_CASE("getint fails on junk",suite) {
+TEST_CASE("getint fails on junk",suite_string) {
 	unsigned int i;
 	REQUIRE_THROWS_AS(i = getint("xx", UNSET),std::runtime_error);
 }
 
-TEST_CASE("getint fails on trailing junk",suite) {
+TEST_CASE("getint fails on trailing junk",suite_string) {
 	unsigned int i;
 	REQUIRE_THROWS_AS(i = getint("xx", UNSET),std::runtime_error);
 }
